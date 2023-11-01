c9e8e5a3782273d30277ba65d59371b88e9f45a7
Chien Tan Kieu (Ryan) <kennyjoe1311@gmail.com>
2023-11-02 01:00:02 +0700

    add standalone output config

--- next.config.js
+++ next.config.js
@@ -1,21 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'standalone',
   images: {
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'avatars.githubusercontent.com',
         port: '',
-        pathname: '/**'
+        pathname: '/**',
       },
       {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
         port: '',
-        pathname: '/**'
-      }
-    ]
-  }
-}
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig
