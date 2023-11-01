5baf60948544c1d920e2c6787f57e6f261a7d600
Chien Tan Kieu (Ryan) <kennyjoe1311@gmail.com>
2023-11-02 03:10:34 +0700

    WIP

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,35 +1,43 @@
 import { PrismaClient } from '@prisma/client';
+// import 'dotenv/config';
 
 const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.user.upsert({
-    where: {
-      email: 'john.doe@prisma.io',
-    },
-    update: {},
-    create: {
-      username: 'john.doe',
-      password: '$2a$07$FGBp3ilWdjWKiFGZEaFun.QD.VvBHuQ79t8hf6gzgNsBy510pf/iy', //123456Abc
-      name: 'John Doe',
-      email: 'john.doe@prisma.io',
-      image: null,
-    },
-  });
+const isDevelopment = process.env.NODE_ENV === 'development';
 
-  await prisma.user.upsert({
-    where: {
-      email: 'mary.jane@prisma.io',
-    },
-    update: {},
-    create: {
-      username: 'mary.jane',
-      password: '$2a$07$RpN5otfwBAO6GVUmxq8X/e7Hh14PjtmMhGaAMFODNnrNsgc4vFBfW', //Abc123456
-      name: 'Mary Jane',
-      email: 'mary.jane@prisma.io',
-      image: null,
-    },
-  });
+async function main() {
+
+  if (isDevelopment) {
+    await prisma.user.upsert({
+      where: {
+        email: 'john.doe@prisma.io',
+      },
+      update: {},
+      create: {
+        username: 'john.doe',
+        password:
+          '$2a$07$FGBp3ilWdjWKiFGZEaFun.QD.VvBHuQ79t8hf6gzgNsBy510pf/iy', //123456Abc
+        name: 'John Doe',
+        email: 'john.doe@prisma.io',
+        image: null,
+      },
+    });
+
+    await prisma.user.upsert({
+      where: {
+        email: 'mary.jane@prisma.io',
+      },
+      update: {},
+      create: {
+        username: 'mary.jane',
+        password:
+          '$2a$07$RpN5otfwBAO6GVUmxq8X/e7Hh14PjtmMhGaAMFODNnrNsgc4vFBfW', //Abc123456
+        name: 'Mary Jane',
+        email: 'mary.jane@prisma.io',
+        image: null,
+      },
+    });
+  }
 
   await prisma.category.createMany({
     skipDuplicates: true,
