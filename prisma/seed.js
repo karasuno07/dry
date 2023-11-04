de6280a9ef0b86ee76a40793c4097ba83fffca34
Chien Tan Kieu (Ryan) <kennyjoe1311@gmail.com>
2023-11-04 20:56:34 +0700

    [completed] multiple env docker image build

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,12 +1,10 @@
-import { PrismaClient } from '@prisma/client';
-// import 'dotenv/config';
+const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 async function main() {
-
   if (isDevelopment) {
     await prisma.user.upsert({
       where: {
@@ -15,8 +13,7 @@ async function main() {
       update: {},
       create: {
         username: 'john.doe',
-        password:
-          '$2a$07$FGBp3ilWdjWKiFGZEaFun.QD.VvBHuQ79t8hf6gzgNsBy510pf/iy', //123456Abc
+        password: '$2a$07$FGBp3ilWdjWKiFGZEaFun.QD.VvBHuQ79t8hf6gzgNsBy510pf/iy', //123456Abc
         name: 'John Doe',
         email: 'john.doe@prisma.io',
         image: null,
@@ -30,8 +27,7 @@ async function main() {
       update: {},
       create: {
         username: 'mary.jane',
-        password:
-          '$2a$07$RpN5otfwBAO6GVUmxq8X/e7Hh14PjtmMhGaAMFODNnrNsgc4vFBfW', //Abc123456
+        password: '$2a$07$RpN5otfwBAO6GVUmxq8X/e7Hh14PjtmMhGaAMFODNnrNsgc4vFBfW', //Abc123456
         name: 'Mary Jane',
         email: 'mary.jane@prisma.io',
         image: null,
