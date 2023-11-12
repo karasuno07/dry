30cace2766b8815513ae74a8e7a13877c3fad3fb
killua <chien.tankieu@gmail.com>
2023-11-12 19:05:54 +0700

    wip

---
+++ src/service/tmdb/categories.ts
@@ -0,0 +1,5 @@
+import { buildAuthorizationHeader } from './utils';
+
+export default class CategoryService {
+  header = buildAuthorizationHeader();
+}
--- src/service/tmdb/utils.ts
+++ src/service/tmdb/utils.ts
@@ -1,9 +1,7 @@
-function buildAuthorizationHeader() {
+export function buildAuthorizationHeader() {
   const accessToken = process.env.TMDB_READ_ACCESS_TOKEN;
   if (!accessToken) {
     throw new Error('Tmdb access token is invalid!');
   }
   return `Bearer ${accessToken}`;
 }
-
-export default class TmdbService {}
