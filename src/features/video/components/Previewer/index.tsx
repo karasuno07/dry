36010c2b58f72a4c06a1f1567490351162e0020c
killua <chien.tankieu@gmail.com>
2023-12-30 03:37:28 +0700

    remove redundant code

--- src/features/video/components/Previewer/index.tsx
+++ src/features/video/components/Previewer/index.tsx
@@ -1,8 +1,4 @@
-import classNames from 'classnames/bind';
 import { VideoType } from 'types/ui';
-import styles from './Previewer.module.scss';
-
-const cx = classNames.bind(styles);
 
 export type PreviewerCommonProps = {
   id: number;
@@ -18,4 +14,3 @@ export type PreviewerCommonProps = {
 export { default as CSRPreviewer } from './CSRPreviewer';
 export { default as SSRPreviewer } from './SSRPreviewer';
 export { default as SkeletonPreviewer } from './Skeleton';
-
