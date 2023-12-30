83deb6051dd898b1d37eb222b343a724c672d5e9
Chien Tan Kieu (Ryan) <kennyjoe1311@gmail.com>
2023-12-31 03:19:49 +0700

    fix wrong import

--- src/components/Navbar/LanguageSwitch.tsx
+++ src/components/Navbar/LanguageSwitch.tsx
@@ -8,7 +8,8 @@ import classNames from 'classnames/bind';
 import { useLocale, useTranslations } from 'next-intl';
 import { useSearchParams } from 'next/navigation';
 import { FaGlobeAmericas, FaGlobeAsia } from 'react-icons/fa';
-import { LocaleType, SUPPORTED_LOCALES } from '~/constants/locales';
+import { LocaleType } from 'types/locale';
+import { SUPPORTED_LOCALES } from '~/constants/locales';
 import styles from './Navbar.module.scss';
 
 const cx = classNames.bind(styles);
