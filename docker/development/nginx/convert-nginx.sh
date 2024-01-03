1d4723671a9bce6b184d9f4f22bbb454b919c189
Chien Tan Kieu (Ryan) <kennyjoe1311@gmail.com>
2024-01-03 22:14:57 +0700

    wip

--- docker/development/nginx/convert-nginx.sh
+++ docker/development/nginx/convert-nginx.sh
@@ -1,5 +1,5 @@
 # !/usr/bin/env bash
 
-envsubst '$${NEXTJS_CONTAINER_IP} ${ADMINER_CONTAINER_ID}' < /etc/nginx/conf.d/nginx.dev.conf > /etc/nginx/conf.d/default.conf 
+envsubst '$${NEXTJS_CONTAINER_IP} ${QBITTORENT_CONTAINER_ID} ${SONARR_CONTAINER_ID} ${RADARR_CONTAINER_ID} ${OVERSEERR_CONTAINER_ID} ${JACKETT_CONTAINER_ID} ${ADMINER_CONTAINER_ID}' < /etc/nginx/conf.d/nginx.dev.conf > /etc/nginx/conf.d/default.conf 
 rm -rf /etc/nginx/conf.d/nginx.dev.conf
 nginx -g "daemon off;"
